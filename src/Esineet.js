6ae6100939ecb05c7e8b8e29c0a50dbd20126847
vappukoskela <vappu.koskela@gmail.com>
2020-11-11 15:02:06 +0200

    added search box

---
+++ src/Esineet.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+
+//Axios, useEffect Context API, usereducer, REDUX
+const Esineet = (props) => {
+    const [data,setData] = useState(["Dataa haetaan"])
+    const [dataNoudettu, setDataNoudettu] = useState(false)
+    const [searchVal, setSearchVal] = useState("")
+    const [valittuPaikkakunta, setValittuPaikkakunta] = useState("")
+    useEffect(()=>{
+        async function haeDataa(){
+            let result = await axios('https://api.huuto.net/1.1/categories/'+props.id+'/items?area='+valittuPaikkakunta
+            )
+            //console.log(result)
+            let a = JSON.parse(result.request.response).items
+
+            setData(JSON.parse(result.request.response).items); 
+            setDataNoudettu(true);   
+            console.log(a)
+        }
+        
+        haeDataa();
+
+    },[props.id, valittuPaikkakunta,searchVal])
+    
+    const paikkakuntaTamperePainettu = ()=>{
+        setValittuPaikkakunta("Tampere")
+    }
+    const  paikkakuntaHelsinkiPainettu = ()=>{
+        setValittuPaikkakunta("Helsinki")
+    }
+
+    const inputFieldHandler = (event) => {
+        console.log(event.target.value)
+        setSearchVal(event.target.value)
+    }
+    
+    return (<div>
+        <button onClick={paikkakuntaTamperePainettu}>Tampere</button>
+        <button onClick={paikkakuntaHelsinkiPainettu}>Helsinki</button>
+        <input onChange={(e) => inputFieldHandler(e)}></input>
+
+        {!dataNoudettu ? "Odotetaan dataa..." :  data.map(alkio=>{
+            if (alkio.title.toUpperCase().includes(searchVal.toUpperCase())){
+                return ( <div>{alkio.title +" Paikassa:"+alkio.location}</div>)
+            }
+        })}
+        </div>)
+
+}
+
+export default Esineet
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App11 from './App11';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App11 />
   </React.StrictMode>,
   document.getElementById('root')
 );
